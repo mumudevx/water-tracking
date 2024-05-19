bb431825403f1e1e10d5791125fb7c7aae213f2f
Mumu <mumudevx@gmail.com>
2024-05-19 18:40:46 +0300

    Project Created
    - Flutter app created via Android Studio.

---
+++ android/app/src/main/kotlin/com/mumudevx/watertracking/water_tracking/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mumudevx.watertracking.water_tracking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Empty Screen App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Empty Screen App'),
+      ),
+      body: const Center(
+        child: Text('This is an empty screen'),
+      ),
+    );
+  }
+}
